645006eca3ee1cb600374a970316cb5fd2cb09cd
Theodor Levi Halldorsson <theodorlevi@protonmail.com>
2025-04-29 11:23:36 +0000

    gallery WIP

--- public/scripts/button.js
+++ public/scripts/button.js
@@ -1,9 +1,18 @@
 const SomethingButton = document.getElementById('something_button');
+const GalleryButton = document.getElementById('gallery');
 
 if (SomethingButton) {
     SomethingButton.addEventListener('click', () => {
         window.location.href = '/something.html';
     });
 } else {
-    console.error('Button with ID "myButton" not found!');
+    console.error('Button with ID "something_button" not found!');
+}
+
+if  (GalleryButton) {
+    GalleryButton.addEventListener('click', () => {
+        window.location.href = '/gallery.html';
+    })
+} else {
+    console.error('Button with ID "gallery" not found!');
 }
